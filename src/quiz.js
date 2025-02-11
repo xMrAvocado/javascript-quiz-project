0051360b01ba855a2483877206029c5058c89b7c
Samuel Perez Besada <samusitesi@gmail.com>
2025-02-11 17:16:40 +0100

    Solved Day 2

--- src/quiz.js
+++ src/quiz.js
@@ -1,43 +1,60 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor (questions, timeLimit, timeRemaining){
-        this.questions = questions; //array de preguntas (array de objetos)
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions; //array de preguntas (array de objetos)
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return (this.currentQuestionIndex += 1);
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = this.questions[i];
+      this.questions[i] = this.questions[j];
+      this.questions[j] = temp;
     }
-    // 2. getQuestion()
-    getQuestion(){
-        return this.questions[this.currentQuestionIndex];
+  }
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (answer === this.questions[this.currentQuestionIndex].answer) {
+      this.correctAnswers += 1;
     }
-    // 3. moveToNextQuestion()
-    moveToNextQuestion(){
-        return this.currentQuestionIndex += 1;
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if ((this.currentQuestionIndex = this.questions.length)) {
+      return true;
     }
-    // 4. shuffleQuestions()
-    shuffleQuestions(){
-        for (let i = this.questions.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            const temp = this.questions[i];
-            this.questions[i] = this.questions[j];
-            this.questions[j] = temp;
+  }
+
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      let filterArr = this.questions.filter((eachQuestion) => {
+        if(difficulty === eachQuestion.difficulty){
+           return true;
         }
+      });
+      this.questions = filterArr;
     }
-    // 5. checkAnswer(answer)
-    checkAnswer(answer){
-        if(answer === this.questions[this.currentQuestionIndex].answer){
-            this.correctAnswers +=1;
-        }
-    }
-    // 6. hasEnded()
-    hasEnded(){
-        if(this.currentQuestionIndex < this.questions.length){
-            return false;
-        }else if(this.currentQuestionIndex = this.questions.length){
-            return true;
-        }
-    }
+  }
+  averageDifficulty(){
+    let averDif = this.questions.reduce((acc, eachQuestion)=>{
+       return acc += eachQuestion.difficulty;
+    }, 0);
+    return averDif/this.questions.length;
+  }
 }
